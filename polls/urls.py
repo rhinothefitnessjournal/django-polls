edd65c5d378836e23010c920d1475f7331d8ac63
Rafael Granja <rafael.granja@aluno.ifsp.edu.br>
2023-06-06 11:28:02 -0300

    enquetes

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("sobre", views.sobre, name="sobre")
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+    return HttpResponse("Página inicial.")
 
-def sobre(request):
-    return HttpResponse("Esta é a página Sobre do meu site.")
+def detail(request, question_id):
+    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Você vai votar na pergunta de número {question_id}.")
