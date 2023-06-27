5cc3ade27368672592b2e4b87a4767a2ec7c5504
Rafael Granja <rafael.granja@aluno.ifsp.edu.br>
2023-06-27 11:20:09 -0300

    5enquetese404

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,20 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Question
+
 def index(request):
-    return HttpResponse("Página inicial.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Enquete inexistente")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
